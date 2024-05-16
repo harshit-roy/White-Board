ec9cdebc9d1295471dbca2a12e2b4aea0a4f305a
Harshit Roy <101564851+harshit-roy@users.noreply.github.com>
2024-05-16 11:17:21 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import io from 'socket.io-client';
 
 import {ToastContainer, toast} from 'react-toastify';
 
-const server = 'https://white-board-collaborator.onrender.com';
+const server = 'https://white-board-collaborator.onrender.com/';
 const connectionOptions = {
   'force new connection': true,
   reconnectionAttempts: 'Infinity',
